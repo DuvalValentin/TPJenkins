4bae83baa74bc731d53d28e113b5e27dab76aaea
valentin <valentin.jw.duval@gmail.com>
2019-09-10 14:32:30 +0200

    commit pour pouvoir push

---
+++ src/main/java/m2ccn/chameauforever/App.java
@@ -0,0 +1,13 @@
+package m2ccn.chameauforever;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
